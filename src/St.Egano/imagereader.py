025053d71ddb6bc43a9f9fbe47eb1817299a3d9b
Lennart 2012 <lennart.schmehr@gmail.com>
2023-11-12 17:27:03 +0100

    safety backup (automaticly)

--- src/St.Egano/imagereader.py
+++ src/St.Egano/imagereader.py
@@ -15,17 +15,16 @@ class ImageReader:
         return self.image
 
     def get_pixel_rgb(self, x, y):
-        # Überprüfen, ob die Koordinaten innerhalb der Grenzen des Bildes liegen
         if 0 <= x < self.width and 0 <= y < self.height:
             rgb = self.image.convert("RGB").getpixel((x, y))
             return rgb
         else:
             raise ValueError("Ungültige Koordinaten")
-    
+
     def get_pixel_values(self, x, y):
-        # Holen Sie sich die RGB-Werte
         rgb = self.get_pixel_rgb(x, y)
-        
-        # Aufteilen in R, G, B
         r, g, b = rgb
         return r, g, b
+    
+    def get_image_dimensions(self):
+        return self.width, self.height
--- src/St.Egano/main.py
+++ src/St.Egano/main.py
@@ -4,21 +4,23 @@ from tkinter import Tk, PhotoImage
 from tkinter.filedialog import askopenfilename
 from PIL import Image
 from imagereader import ImageReader
-
+print('All work is copyrighted by GNU GENERAL PUBLIC LICENSE. View the License.exe for more information.')
 
 root = Tk()
 root.withdraw()
 logo_path = "img/icon.png"
 logo = PhotoImage(file=logo_path)
+running = True
+
 
 def get_image_path():
     if len(sys.argv) > 1:
         return sys.argv[1]
     else:
-        
         root.tk.call('wm', 'iconphoto', root._w, logo)
 
-        file_path = askopenfilename(title="Bild auswählen", filetypes=[("Images", "*.png;*.jpg;*.jpeg;*.gif;*.bmp;*.tiff;*.tif")])
+        file_path = askopenfilename(title="Bild auswählen", filetypes=[(
+            "Images", "*.png;*.jpg;*.jpeg;*.gif;*.bmp;*.tiff;*.tif")])
 
         if not file_path:
             print("Kein Bild ausgewählt. Programm wird beendet.")
@@ -27,15 +29,29 @@ def get_image_path():
         return file_path
 
 
-
 if __name__ == "__main__":
-    x=0
-    y=0
-    
     image_path = get_image_path()
     image_reader = ImageReader(image_path)
     loaded_image = image_reader.load_image()
-    
-    r, g, b = image_reader.get_pixel_values(x, y)
 
-    print(f"R: {r}, G: {g}, B: {b}")
+    # Hier werden die Bildbreite und Bildhöhe abgerufen
+    image_width, image_height = image_reader.get_image_dimensions()
+
+    x = 0
+    y = 0
+    while running:
+        r, g, b = image_reader.get_pixel_values(x, y)
+        print(r, g, b)
+
+        if b == 0 and g == 0:
+            running = False
+
+        x += g
+        y += b
+
+        for xvalue in range(y0, x):
+            if x == image_width:
+                x = 0
+            x += 1
+
+        print(f"continuing, x={x} y={y}")
