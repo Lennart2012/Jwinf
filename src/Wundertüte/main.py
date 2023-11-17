beb9f496d6a08d76116532cbea0484f2ac014646
Lennart 2012 <lennart.schmehr@gmail.com>
2023-11-17 17:30:35 +0100

    started Wundertüte

--- src/Wundertüte/main.py
+++ src/Wundertüte/main.py
@@ -1,7 +1,3 @@
+# in main.py
 from userinputs import ask_all as get_inputs
 inputs = get_inputs()
-
-
-for lucky_bag in range(0, inputs[0]):
-    print("Test")
-    print(lucky_bag)
--- src/Wundertüte/userinputs.py
+++ src/Wundertüte/userinputs.py
@@ -1,33 +1,40 @@
-#python module to get the user inputs
+# in userinputs.py
+# python module to get the user inputs
 userinputs = []
-#ask for all amounts (main function in module)
+
+# ask for all amounts (main function in module)
+
+
 def ask_all():
-    ask("Lucky bags")
-    ask("Card games")
-    ask("Dice games")
-    ask("Skill games")
-    
-    #ask if continue
-    print("You entered following amounts:" + " Amount of lucky bags:" + userinputs[0] + " Amount of card games:" + userinputs[1] + " Amount of dice games:" + userinputs[2] + " Amount of skill games:" + userinputs[3])  
-    Continue = input("Do you want to continue (Y) or correct a input (N)?")
+    ask("Amount of lucky bags")
+    ask("Amount of card games")
+    ask("Amount of dice games")
+    ask("Amount of skill games")
+
+    # ask if continue
+    print(
+        f"You entered following amounts: Amount of lucky bags:{userinputs[0]} - Amount of card games:{userinputs[1]} - Amount of dice games:{userinputs[2]} - Amount of skill games:{userinputs[3]}")
+    Continue = input("Do you want to continue (Y) or correct an input (N)?")
     if Continue == "y" or Continue == "Y":
         return userinputs
     elif Continue == "n" or Continue == "N":
-        ask()
-        
-    
-#get amount of lucky bags
+        ask_all()
+
+# get amount of lucky bags
+
+
 def ask(what):
-    userinput = input("amount of " + what)
-    #check if input is vaild
+    userinput = input(f"{what}: ")
+    # check if input is valid
     if userinput.isdigit():
-        #if yes add input to answer list
-        userinputs.append(userinput)
+        # if yes add input to answer list
+        userinputs.append(int(userinput))
     else:
-        #if no restart the user input function
-        print("You didn't entered a vaild number. Please enter a number.")
+        # if no restart the user input function
+        print("You didn't enter a valid number. Please enter a number.")
         ask(what)
 
-#run user input functions if code is not runned as module
+
+# run user input functions if code is not runned as module
 if __name__ == "__main__":
-    print(ask())
+    print(ask_all())
