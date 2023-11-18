3bf4f82ce9e9b3c6cdd0656f58fab545f764c97a
Lennart 2012 <lennart.schmehr@gmail.com>
2023-11-18 12:42:08 +0100

    backup 17/11/23

--- src/Wundertüte/main.py
+++ src/Wundertüte/main.py
@@ -1,6 +1,6 @@
 # in main.py
-from userinputs import *
-from textcontrol import *
+from userinputs import UserInput
+from textcontrol import textcontrol
 
 inputs = UserInput.ask_all()
 print(inputs)
@@ -17,3 +17,12 @@ for dice_game in range(1, inputs[1]+1):
 
     textcontrol.add_data(line, "Dice Game", id)
     id += 1
+
+if line != inputs[0]:
+    for i in range(line+1, inputs[0]+1):
+        textcontrol.add_data(i, "missed", id)
+        id += 1
+
+
+print(textcontrol.get_line_data(2))
+if 
--- src/Wundertüte/textcontrol.py
+++ src/Wundertüte/textcontrol.py
@@ -30,32 +30,70 @@ class textcontrol():
             file.writelines(lines)
 
     @staticmethod
-    def remove_data(line, id):
+    def remove_data(id):
         file_path = 'outputs.txt'
-        if id == None:
+        if id is None:
             raise ValueError
+
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+
+        for i, line in enumerate(lines):
+            pattern = re.compile(rf'\[{id};.*?\]')
+            lines[i] = re.sub(pattern, '', line)
+
+        with open(file_path, 'w') as file:
+            file.writelines(lines)
+
+        print(f"Text in eckigen Klammern für ID {id} gelöscht.")
+
+    @staticmethod
+    def get_line_data(line):
+        file_path = 'outputs.txt'
+
         # Lese alle Zeilen aus der Datei
         with open(file_path, 'r') as file:
             lines = file.readlines()
 
         # Überprüfe, ob die Zeile existiert
         if 1 <= line <= len(lines):
-            # Suche nach Text in eckigen Klammern in der gewünschten Zeile und lösche ihn
-            pattern = re.compile(rf'\[{id};.*?\]')
-            lines[line - 1] = re.sub(pattern, '', lines[line - 1])
+            # Suche nach Text in eckigen Klammern in der gewünschten Zeile
+            data_in_brackets = re.findall(r'\[\d+;(.*?)\]', lines[line - 1])
 
-            # Schreibe alle aktualisierten Zeilen zurück in die Datei
-            with open(file_path, 'w') as file:
-                file.writelines(lines)
-            print(
-                f"Text in eckigen Klammern für Zeile {line} und ID {id} gelöscht.")
+            # Gib das gefundene Daten-Array zurück
+            return data_in_brackets
         else:
             print(f"Die Zeile {line} existiert nicht in der Datei.")
+            return None
+
+    @staticmethod
+    def get_id_for_data_in_line(line, data):
+        file_path = 'outputs.txt'
+
+        # Lese alle Zeilen aus der Datei
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+
+        # Überprüfe, ob die Zeile existiert
+        if 1 <= line <= len(lines):
+            # Suche nach der ID für den angegebenen Text in eckigen Klammern in der gewünschten Zeile
+            pattern = re.compile(rf'\[(\d+);{re.escape(data)}\]')
+            match = re.search(pattern, lines[line - 1])
+
+            # Wenn ein Übereinstimmung gefunden wurde, gib die ID zurück
+            if match:
+                return int(match.group(1))
+            else:
+                print(f"Der Text {data} wurde nicht in Zeile {line} gefunden.")
+                return None
+        else:
+            print(f"Die Zeile {line} existiert nicht in der Datei.")
+            return None
 
 
 if __name__ == '__main__':
     textcontrol.create_framework(11)
-    textcontrol.add_data(7, "Test")
-    textcontrol.add_data(7, "Test")
+    textcontrol.add_data(7, "Test", 1)
+    textcontrol.add_data(7, "Test", 2)
     input()
-    textcontrol.remove_data(7, 1)
+    textcontrol.remove_data(1)
